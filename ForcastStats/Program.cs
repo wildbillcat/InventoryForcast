f68790e62c9a1cb62ebb0ba1aa45fd0c35670e3d
Patrick McMorran <kingrolloinc@sbcglobal.net>
2015-11-20 20:22:15 -0500

    test cmd

---
+++ ForcastStats/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StatsCalc;
+
+namespace ForcastStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new List<double> {0.0, 0.0, 0.0, 0.0};
+            double[] yValues = new double[] { 6, 7, 9, 15, 21 };
+            double[] xValues = new double[] { 20, 28, 31, 38, 40 };
+            double xValueToPredict = 30;
+
+            double[] t = Linear.Forcast(xValueToPredict, xValues, yValues);
+            System.Console.WriteLine(t[2]);
+            System.Console.ReadLine();
+
+            System.Console.WriteLine((DateTime.Now.Date.Year * 12 * DateTime.Now.Date.Month));
+            System.Console.ReadLine();
+        }
+    }
+}
