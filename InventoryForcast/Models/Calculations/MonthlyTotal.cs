10a1302c21b2ed9434a82770fba28779d53e30e2
Patrick McMorran <kingrolloinc@sbcglobal.net>
2015-12-19 23:26:06 -0500

    rethought how to store the data after sketching the layout

--- InventoryForcast/Models/Calculations/MonthlyTotal.cs
+++ InventoryForcast/Models/Calculations/MonthlyTotal.cs
@@ -9,12 +9,14 @@ namespace InventoryForcast.Models.Calculations
 {
     public class MonthlyTotal
     {
-        [Key]
+        [Key, Column(Order = 0)]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int SKU { get; set; }
+        [Key, Column(Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Month_Id { get; set; } //Month * Year
         public double Quantity_Sold { get; set; }
         public double Absolute_Quantity_Sold { get; set; }
         public DateTime Date { get; set; }
-        public int Month_Id { get; set; }
     }
 }
--- InventoryForcast/Models/Calculations/SingleLinearForcast.cs
+++ InventoryForcast/Models/Calculations/SingleLinearForcast.cs
@@ -7,19 +7,22 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace InventoryForcast.Models.Calculations
 {
-    public class ForcastValue
+    public class SingleLinearForcast
     {
-        [Key]
+        [Key, Column(Order = 0)]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int SKU { get; set; }
+        [Key, Column(Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Month_Id { get; set; } //Month * Year
+        public DateTime Date { get; set; } //Forcasted Date
         public double Quantity_Forcast { get; set; }
         public double Absolute_Quantity_Sold { get; set; }
-        public DateTime Date { get; set; } //Forcasted Date
-        public int Month_Id { get; set; } //Forcasted Date
         public double Slope { get; set; }
         public double Intercept { get; set; }
         public string JSON_MonthlyTotals { get; set; }
         public int Sample_Size { get; set; }
+        public string SkuClass { get; set; }
         public bool Valid { get; set; } //Used if not enough data exists to properly generate this calculation.
     }
 }
