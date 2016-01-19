8d601ec3f5c54297085ed6bcbae4b072cd2b144c
Patrick McMorran <kingrolloinc@sbcglobal.net>
2016-01-19 01:22:43 -0500

    split

---
+++ InventoryForcast/Resources/Dates-Split.ps1
@@ -0,0 +1,8 @@
+﻿$file = Get-Content -Path "C:\Users\wildbillcat\Sync\WebSpecs\ReportRefactor.csv"
+
+$headers = $file[0].Split(',')
+
+$Dates = @()
+for($i = 1; $i -lt $headers.length; $i++){
+    Set-Content -Value $file -Path ("C:\Users\wildbillcat\Sync\WebSpecs\SKUs\" + (get-date ("1-"+$headers[$i])).ToString("MM-dd-yyyy") + ".csv")
+}
